bb1281c59c90ff364371d0ffea37f9c8174c93e1
Arjumand <4020arjumandrashid@gmail.com>
2025-07-25 19:26:08 +0530

    Additional projects

--- proj_details.js
+++ proj_details.js
@@ -2,20 +2,28 @@ document.addEventListener("DOMContentLoaded", function (){
     const projects = [
         {
             id: 1,
+            title: "Skinwise: Your Agentic AI Skincare Assistant",
+            longDescription: "Skinwise is an interactive, full-stack AI skincare assistant developed using multi-agent orchestration (PhiData + Groq), tailored to answer questions reagrding skincare. It feature automated query classification and agent routing via Wikipedia, SerpAPI and web crawler agents to provide tailored insights. The React framework provides it a sleek UI along with a FLask backend.",
+            image: "skincare_5.png",
+            github: "https://github.com/arjumand252/Skinwise_Agentic_AI",
+            // video: "skincare_5.png"
+        },
+        {
+            id: 2,
             title: "ToxNet: A Multi-layer Network approach to Toxicity Analysis",
             longDescription:"ToxNet is a deep learning model based on the Graph Neural Networks Architecture, capable of categorizing tweets into their correct toxicity label. It constructs a multi-layered network out of the tweets to exploit their syntactic and semantic information. The research paper for this project has been accepted and presented in the Sixth International Conference on Soft Computing and its Engineering Applications, 2024.",
             image: "proj1_img.jpeg",
             github: "https://github.com/raj248/toxnet_research_code"
         },
         {
-            id: 2,
+            id: 3,
             title: "Drowsy Driver Detection",
             longDescription: "This project employes Computer vision for the task of detecting whether a driver is drowsy or alert by evaluating their image. It employe the powerful HAAR Cascade Classifier for image segmentation task and a deep CNN model to determine the alertness of the driver.",
             image: "proj2_img.png",
             github: "https://github.com/arjumand252/Drowsy-Driver-Detection-using-CNN-and-HAAR-Cascade-Classifier"
         },
         {
-            id: 3,
+            id: 4,
             title: "Mental Health Condition Prediction using Social Media Analysis",
             longDescription: "This project provides users with a convinient interface consisting of a basic questionnaire regarding their social media usage. Based on the user's answers, the projects employs an ensemble of 3 models: Fully Connected Nerual Network, AutoEncoder and ResNet. These models provide a multi-label classification of the user's mental health condition.",
             image: "images/project3.jpg",
--- script.js
+++ script.js
@@ -4,9 +4,10 @@ document.getElementById("download-resume").addEventListener("click", () => {
 
 document.addEventListener("DOMContentLoaded", function () {
     const projects = [
-        { title: "ToxNet: A Multi-layer Network approach to Toxicity Analysis", image: "proj1_img.jpeg", link: "project.html?id=1" },
-        { title: "Drowsy Driver Detection using Computer Vision", image: "proj2_img_index.png", link: "project.html?id=2" },
-        { title: "Mental Health Prediction using Social Media Analysis", image: "proj3_img_index.png", link: "project.html?id=3" }
+        { title: "Skinwise: Your Agentic AI Skincare Assistant", image: "skincare_3.png", link: "project.html?id=1"},
+        { title: "ToxNet: A Multi-layer Network approach to Toxicity Analysis", image: "proj1_img.jpeg", link: "project.html?id=2" },
+        { title: "Drowsy Driver Detection using Computer Vision", image: "proj2_img_index.png", link: "project.html?id=3" },
+        { title: "Mental Health Prediction using Social Media Analysis", image: "proj3_img_index.png", link: "project.html?id=4" }
     ];
 
     const projectList = document.getElementById("project-list");
